8a941804eb643339a104e2e76db9788f57c0cb7d
Simon Labrecque <simon@wegel.ca>
2013-09-19 23:17:01 -0400

    Added FluentMappingsContainer.Add() and .Add<T>()

--- FluentProtobufNet/FluentMappingsContainer.cs
+++ FluentProtobufNet/FluentMappingsContainer.cs
@@ -6,17 +6,51 @@ namespace FluentProtobufNet
 {
     public class FluentMappingsContainer
     {
-        readonly IList<Assembly> assemblies = new List<Assembly>();
-        readonly List<Type> types = new List<Type>();
+        readonly IList<Assembly> _assemblies = new List<Assembly>();
+        readonly List<Type> _types = new List<Type>();
 
+        /// <summary>
+        /// Add all fluent mappings in the assembly that contains T.
+        /// </summary>
+        /// <typeparam name="T">Type from the assembly</typeparam>
+        /// <returns>Fluent mappings configuration</returns>
         public FluentMappingsContainer AddFromAssemblyOf<T>()
         {
             return AddFromAssembly(typeof(T).Assembly);
         }
 
+        /// <summary>
+        /// Add all fluent mappings in the assembly
+        /// </summary>
+        /// <param name="assembly">Assembly to add mappings from</param>
+        /// <returns>Fluent mappings configuration</returns>
         public FluentMappingsContainer AddFromAssembly(Assembly assembly)
         {
-            assemblies.Add(assembly);
+            _assemblies.Add(assembly);
+            WasUsed = true;
+            return this;
+        }
+
+        /// <summary>
+        /// Adds a single <see cref="IMappingProvider" /> represented by the specified type.
+        /// </summary>
+        /// <returns>Fluent mappings configuration</returns>
+        public FluentMappingsContainer Add<T>()
+        {
+            return Add(typeof(T));
+        }
+
+        /// <summary>
+        /// Adds a single <see cref="IMappingProvider" /> represented by the specified type.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <returns>Fluent mappings configuration</returns>
+        public FluentMappingsContainer Add(Type type)
+        {
+            if (type == null)
+                throw new ArgumentNullException("type");
+
+            _types.Add(type);
             WasUsed = true;
             return this;
         }
@@ -25,9 +59,14 @@ namespace FluentProtobufNet
 
         internal void Apply(PersistenceModel model)
         {
-            foreach (var assembly in assemblies)
+            foreach (var assembly in _assemblies)
             {
                 model.AddMappingsFromAssembly(assembly);
+            }
+
+            foreach (var type in _types)
+            {
+                model.Add(type);
             }
         }
     }
